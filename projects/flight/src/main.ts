57278a84c4a13f72bd1f4d3c63816f39c3c9dcf0
mikezks <23002661+mikezks@users.noreply.github.com>
2024-11-19 10:38:49 +0100

    refactor: app config

--- projects/flight/src/main.ts
+++ projects/flight/src/main.ts
@@ -1,23 +1,7 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app/app.component';
-import { APP_ROUTES } from './app/app.routes';
-import { routerFeature } from './app/shared/logic-router-state';
+import { appConfig } from './app/app.config';
 
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(
-          StoreModule.forRoot(),
-          EffectsModule.forRoot(),
-          StoreModule.forFeature(routerFeature)
-        ),
-        provideRouter(APP_ROUTES),
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-})
+bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
