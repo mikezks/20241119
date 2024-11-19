d7815d816c70de098abc96a8a4763c962ab6ad22
mikezks <23002661+mikezks@users.noreply.github.com>
2024-11-19 13:23:00 +0100

    feat: router provide with features

--- projects/flight/src/app/app.config.ts
+++ projects/flight/src/app/app.config.ts
@@ -10,7 +10,9 @@ import { provideRouterFeature } from './shared/logic-router-state';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(APP_ROUTES,
-      withComponentInputBinding()
+      withComponentInputBinding(),
+      // withDebugTracing(),
+      // withDisabledInitialNavigation()
     ),
     provideHttpClient(),
     provideStore(),
