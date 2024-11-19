92a1462f657c220fadc1a14750a17b95f66ff76a
mikezks <23002661+mikezks@users.noreply.github.com>
2024-11-19 10:36:36 +0100

    refactor: app bootstrapping, app routing

--- projects/flight/src/main.ts
+++ projects/flight/src/main.ts
@@ -1,6 +1,23 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { AppComponent } from './app/app.component';
+import { APP_ROUTES } from './app/app.routes';
+import { routerFeature } from './app/shared/logic-router-state';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(
+          StoreModule.forRoot(),
+          EffectsModule.forRoot(),
+          StoreModule.forFeature(routerFeature)
+        ),
+        provideRouter(APP_ROUTES),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
