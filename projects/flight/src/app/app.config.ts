0b8d4dd4e5f691caf4e45b884c044e6d714b576e
mikezks <23002661+mikezks@users.noreply.github.com>
2024-11-19 14:29:58 +0100

    feat: provide app initializer

--- projects/flight/src/app/app.config.ts
+++ projects/flight/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, inject, provideAppInitializer } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
@@ -7,6 +7,8 @@ import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { APP_ROUTES } from './app.routes';
 import { provideRouterFeature } from './shared/logic-router-state';
 import { authInterceptor } from './shared/logic-communication/http-interceptors/auth.interceptor';
+import { delay, of, tap } from 'rxjs';
+import { FlightService } from './booking/api-boarding';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -25,6 +27,22 @@ export const appConfig: ApplicationConfig = {
     provideStore(),
     provideEffects(),
     provideRouterFeature(),
-    provideStoreDevtools()
+    provideStoreDevtools(),
+    /* {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: (
+        flightService = inject(FlightService)
+      ) => () => of(true).pipe(
+        delay(5_000),
+        tap(() => console.log(flightService.flights))
+      )
+    }, */
+    provideAppInitializer((
+      flightService = inject(FlightService)
+    ) => of(true).pipe(
+      delay(5_000),
+      tap(() => console.log(flightService.flights))
+    ))
   ]
 };
