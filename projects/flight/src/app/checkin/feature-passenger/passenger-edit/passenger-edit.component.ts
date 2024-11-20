eb0bac1c11e5e4fbfbc7ef23a0a426c32a14b8a5
mikezks <23002661+mikezks@users.noreply.github.com>
2024-11-20 11:32:59 +0100

    refactor: rxjs interop toSignal toObservable

--- projects/flight/src/app/checkin/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ projects/flight/src/app/checkin/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,18 +1,35 @@
+import { PassengerService } from './../../logic-passenger/data-access/passenger.service';
 import { NgIf } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, input, numberAttribute, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { validatePassengerStatus } from '../../util-validation';
+import { RouterLink } from '@angular/router';
+import { initialPassenger } from '../../logic-passenger';
+import { switchMap } from 'rxjs';
 
 
 @Component({
   selector: 'app-passenger-edit',
   imports: [
     NgIf,
+    RouterLink,
     ReactiveFormsModule
   ],
   templateUrl: './passenger-edit.component.html'
 })
 export class PassengerEditComponent {
+  private readonly passengerService = inject(PassengerService);
+
+  protected readonly id = input.required({ transform: numberAttribute });
+  private readonly id$ = toObservable(this.id);
+  private passenger$ = this.id$.pipe(
+    switchMap(id => this.passengerService.findById(id))
+  );
+  protected readonly passenger = toSignal(this.passenger$, {
+    initialValue: initialPassenger
+  });
+
   protected editForm = inject(NonNullableFormBuilder).group({
     id: [0],
     firstName: [''],
@@ -22,6 +39,11 @@ export class PassengerEditComponent {
       validatePassengerStatus(['A', 'B', 'C'])
     ]]
   });
+
+  constructor() {
+    effect(() => console.log(this.id()));
+    effect(() => this.editForm.patchValue(this.passenger()));
+  }
 
   protected save(): void {
     console.log(this.editForm.value);
